697fb6db8acd5ab533f923d463926a4d0896d4b2
Kirill Shvakov <shvakov@gmail.com>
2016-05-19 00:17:26 +0300

    --

--- assets/static/js/app.js
+++ assets/static/js/app.js
@@ -1,32 +1,32 @@
 $('#confirmAction').on('show.bs.modal', function (event) {
 
-  var button = $(event.relatedTarget);
-  var action = button.data('action');
+    var button = $(event.relatedTarget);
  
-  $(this).find('.modal-content form').on('submit', function(event) {
-
-        $.ajax({
-            method   : 'POST',
-            url      : action,
-            data     : $(this).serialize(),
-            dataType : 'json',
-            success  : function(data, event) {
-                location.reload(true);
-            },
-            error: function(request, status, error) {
-              
-                switch (request.status) {
-                    case 403:
-                        alert("Authentication failed");
-                        break;
-                    default:
-                        alert(error);
-                        break;
-                }
-            }
-        });
-
-        return false
-    });
+    $(this).find('.modal-content form').attr('action', button.data('action'));
 });
 
+$('#confirmAction').find('.modal-content form').on('submit', function(event) {
+
+    $.ajax({
+        method   : 'POST',
+        url      : $(this).attr('action'),
+        data     : $(this).serialize(),
+        dataType : 'json',
+        success  : function(data, event) {
+            location.reload(true);
+        },
+        error: function(request, status, error) {
+          
+            switch (request.status) {
+                case 403:
+                    alert("Authentication failed");
+                    break;
+                default:
+                    alert(error);
+                    break;
+            }
+        }
+    });
+
+    return false
+});
