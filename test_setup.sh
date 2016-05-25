5d7f98788eb06bfa39c06ccb7640f83719feea59
kshvakov <shvakov@gmail.com>
2016-05-25 23:23:33 +0300

    view build page

--- test_setup.sh
+++ test_setup.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-echo "Get gvt (https://github.com/FiloSottile/gvt)"
-
-go get -u github.com/FiloSottile/gvt
-
 echo "Restore vendors"
 
 gvt restore
-mkdir -p /go/src/github.com/postgres-ci/app-server
+
+mkdir -p /go/src/github.com/postgres-ci/app-server/
+
 cp -r . /go/src/github.com/postgres-ci/app-server/
